3f2a27f254796f03a99c1dbea22518e2758e6c1a
cybergh0st7 <tundx0@gmail.com>
2023-02-27 02:36:57 +0100

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get('/', (req,res) => {
+    res.json({
+        hello: 'world'
+    });
+});
+const port = Number(process.env.PORT || 3333);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server started at http://localhost:${port}`)
+});
